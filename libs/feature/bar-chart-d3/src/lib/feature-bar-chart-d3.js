64968bb4b7170eb23f986dbf5fe12a2cae09ac54
Maciej Drahusz <maciej.drahusz@stepwise.pl>
2021-09-10 14:40:31 +0200

    bc d3

---
+++ libs/feature/bar-chart-d3/src/lib/feature-bar-chart-d3.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+const StyledFeatureBarChartD3 = styled.div`
+  color: pink;
+`;
+export function FeatureBarChartD3(props) {
+  return (
+    <StyledFeatureBarChartD3>
+      <h1>Welcome to FeatureBarChartD3!</h1>
+    </StyledFeatureBarChartD3>
+  );
+}
+export default FeatureBarChartD3;
